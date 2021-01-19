d70bc2d697d9159d91a9767c5a8ae49a2a012f61
gabriel-demes <Gabrieldemes@nyu.edu>
2021-01-19 14:04:37 -0500

    driver 2

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,3 +1,36 @@
 class Manager
 
+    attr_accessor :name, :age, :department
+    @@all = []
+    def initialize(name, age, department)
+        self.name = name
+        self.age = age
+        self.department = department
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def roles
+        Role.all.select{|role| role.manager == self}
+    end
+
+    def employees
+        roles.map{|role| role.employee}.uniq
+    end
+
+    def self.all_departments
+        Role.all.map{|role| role.manager.department}.uniq
+    end
+
+    def self.average_age
+        all.sum{|manager| manager.age} / all.count.to_f
+    end
+
+    
+
+
+
 end
