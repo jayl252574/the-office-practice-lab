ddbfb73fc48bee2cc1304b7b661a5bc5bc9372e1
ajperlis07 <ajperlis07@gmail.com>
2021-01-19 13:54:47 -0500

    role complete

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,29 @@
 class Role
-    
+    @@all = []
+    attr_reader :name, :manager, :employee
+    def initialize(name, manager, employee)
+        @name = name 
+        @manager = manager
+        @employee = employee
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        all.select {|employee| role.employee == name}.map {|role| role.employee}.uniq
+        #all.map(&:employee)
+    end
+
+    def managers
+        Role.all.select {|role| role.name == name}.map {|role| role.manager}.uniq
+    end
+
+    def add_employee(new_name, new_salary, manager)
+        new_employee = Employee.new(new_name, new_salary)
+        Role.new(self.name, manager, new_employee)
+    end
+
 end
